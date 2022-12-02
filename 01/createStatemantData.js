ad63d66fa492fe5c10ce376bfac5d3f998dd9dde
rulewrite <rulewrite@gmail.com>
2022-12-02 22:46:10 +0900

    refactor: 공연료 계산 로직 계산기 클래스로 이전

--- 01/createStatemantData.js
+++ 01/createStatemantData.js
@@ -1,4 +1,26 @@
 class PerformanceCalculator {
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case 'tragedy': // 비극
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case 'comedy': // 희극
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+    return result;
+  }
+
   constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
     this.play = aPlay;
@@ -28,35 +50,13 @@ function createStatementData(invoice, plays) {
     );
     const result = Object.assign({}, aPerformance); // 얕은 복사
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case 'tragedy': // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy': // 희극
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result;
   }
 
   function volumeCreditsFor(aPerformance) {
