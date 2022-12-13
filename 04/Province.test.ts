61bfd7c6ccac286865d62c052c99044d38b7bb85
rulewrite <rulewrite@gmail.com>
2022-12-14 01:53:58 +0900

    test: 중복되는 테스트 픽스처 생성 제거

--- 04/Province.test.ts
+++ 04/Province.test.ts
@@ -14,13 +14,23 @@ const sampleProvinceData = () => {
 };
 
 describe('province', () => {
+  /**
+   * Don't
+   * 테스트 관련 버그 중 가장 지저분한 유형 "테스트 끼리 상호작용하게 하는 공유 픽스처"를 생성하는 원인.
+   * 즉, 테스트를 실행하는 순서에 따라 결과가 달라질 수 있다.
+   * const asis = new Province(sampleProvinceData());
+   */
+  // 테스트마다 독립적인 픽스처가 생성되도록
+  let asis: Province;
+  beforeEach(() => {
+    asis = new Province(sampleProvinceData());
+  });
+
   test('shortfall', () => {
-    const asis = new Province(sampleProvinceData());
     expect(asis.shortfall).toBe(5);
   });
 
   test('profit', () => {
-    const asis = new Province(sampleProvinceData());
     expect(asis.profit).toBe(230);
   });
 });
